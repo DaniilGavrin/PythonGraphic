413b7247f4854f30546774f9d4c635a13456d459
Daniil <bksbank@bk.ru>
2023-05-09 12:38:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 #создаем массивы данных
-x = np.linspace(0, 10, 100) #массив значений по оси х
+x = np.linspace(0, 10, 50) #массив значений по оси х
 y = np.sin(x) #массив значений по оси y
+
+#создаём график
+plt.plot(x, y)
+
+#настрлйка параметров графика
+plt.title('Синусоидальная функция')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+
+plt.grid(True)
+
+#Выводим график на экран
+plt.show()
