3a034cac186934ac2b3129ca62b734114a4e9d3c
Daniil <bksbank@bk.ru>
2023-05-09 21:11:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import numpy as np
 # Создаём данные для графика
 names = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
 
-employee1 = [8, 7, 6, 10 9, 2, 0]
-employee1 = [8, 7, 6, 8, 9, 4, 0]
-employee1 = [8, 7, 6, 8, 9, -1, 0]
+employee1 = [0, 1, 0, 0, 0, 0, 0]
+employee2 = [0, 0, 0, 0, 0, 0, 0]
+employee3 = [0, 0, 0, 0, 0, 0, 0]
 
 #Создаем график
-fig, ax = plt.sup
+fig, ax = plt.subplots()
+bar_width = 0.25
+opacity = 0.8
+index = np.arange(len(names))
+
+rects1 = plt.bar(index, employee1, bar_width, alpha=opacity, color='b', label = 'Даниил')
+rects2 = plt.bar(index + bar_width, employee2, bar_width, alpha=opacity, color='g', label = 'Лера')
+rects3 = plt.bar(index + bar_width *2, employee3, bar_width, alpha=opacity, color='r', label = 'Нет сотрудника')
+
+plt.xlabel('День недели')
+plt.ylabel('Часы работа')
+plt.title('Время работы сотрудников по дням недели')
+plt.xticks(index + bar_width, names)
+plt.legend()
+
+plt.tight_layout()
+plt.show()
